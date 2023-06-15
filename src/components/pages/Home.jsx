dfe67b84f28ae023a45f84acf742eedd0a2d6235
Carlos David <apolayamendozacarlos@gmail.com>
2023-06-14 19:56:38 -0500

    Se agrega Router, links al menú y la configuración inicial de TailwindCSS

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { API_URL } from "../../constants/env"
+
+function Home() {
+
+  return (
+    <>
+      <div>
+        <h1>Hola Mundo {API_URL} </h1>
+        <p>Sitio en desarrollo</p>
+      </div>
+    </>
+  )
+}
+
+export default Home
